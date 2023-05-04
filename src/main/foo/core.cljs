20278a3a6762f7613460fc4b145f316f4d3b546f
Mario Giampietri <mario.giampietri@gmail.com>
2023-05-04 21:56:32 +0100

    wrap up

--- src/main/foo/core.cljs
+++ src/main/foo/core.cljs
@@ -1,60 +1,35 @@
-(ns foo.core)
+(ns foo.core
+  (:require [reagent.core :as r]
+            [reagent.dom :as rdom]))
 
 ;; STATE
-(defonce state (atom 0))
 
-;; HELPERS
-(defn root []
-  (.getElementById js/document "app"))
-
-(defn counter []
-  (.getElementById js/document "counter"))
-
-(defn replaceChild [root new-child old-child]
-  (.replaceChild root new-child old-child))
+(defonce state (r/atom 0))
 
 ;; COMPONENTS
-(defn new-counter []
-  (doto (.createElement js/document "p")
-    (.setAttribute "id" "counter")
-    (aset "innerHTML" @state)))
 
-(defn new-button-inc []
-  (doto (.createElement js/document "button")
-    (aset "innerHTML" "UNASSSS AINASSS")
-    (aset "onclick" (fn [_event]
-                      (swap! state inc)
-                      (replaceChild (root) (new-counter) (counter))))))
+(defn counter []
+  [:p "Kounter: " @state])
 
-(defn new-button-reset []
-  (doto (.createElement js/document "button")
-    (aset "innerHTML" "reset")
-    (aset "onclick" (fn [_event]
-                      (reset! state 0)
-                      (replaceChild (root) (new-counter) (counter))))))
+(defn button-inc []
+  [:input {:type "button"
+           :value "+1"
+           :on-click (fn [_event]
+                       (swap! state inc))}])
 
-(defn new-label []
-  (doto (.createElement js/document "label")
-    (aset "innerHTML" (js/Date.))))
+(defn button-reset []
+  [:input {:type "button"
+           :value "reset"
+           :on-click (fn [_event]
+                       (reset! state 0))}])
 
-(comment
-
-  (macroexpand-1 (doto (.createElement js/document "label")
-    (aset "innerHTML" (js/Date.))))
-  ,)
+(defn container []
+  [:div
+   [counter]
+   [button-inc]
+   [button-reset]])
 
 ;; RENDER
-(defn render [root & components]
-  (doseq [c components]
-    (.append root c)))
 
-(defn ^:export init
-  []
-  (aset (root) "innerHTML" "")
-  (render (root) (new-counter) (new-button-inc) (new-button-reset) (new-label)))
-
-(comment
-  (+ 1 1)
-  (reset! state 42)
-(init)
-  )
+(defn ^:export init []
+  (rdom/render [container] (.getElementById js/document "app")))
---
+++ src/main/foo/core_vanilla.cljs
@@ -0,0 +1,48 @@
+(ns foo.core-vanilla)
+
+;; STATE
+(defonce state (atom 0))
+
+;; HELPERS
+(defn root []
+  (.getElementById js/document "app"))
+
+(defn counter []
+  (.getElementById js/document "counter"))
+
+(defn replaceChild [root new-child old-child]
+  (.replaceChild root new-child old-child))
+
+;; COMPONENTS
+(defn new-counter []
+  (doto (.createElement js/document "p")
+    (.setAttribute "id" "counter")
+    (aset "innerHTML" @state)))
+
+(defn new-button-inc []
+  (doto (.createElement js/document "button")
+    (aset "innerHTML" "+1")
+    (aset "onclick" (fn [_event]
+                      (swap! state inc)
+                      (replaceChild (root) (new-counter) (counter))))))
+
+(defn new-button-reset []
+  (doto (.createElement js/document "button")
+    (aset "innerHTML" "reset")
+    (aset "onclick" (fn [_event]
+                      (reset! state 0)
+                      (replaceChild (root) (new-counter) (counter))))))
+
+#_(defn new-label []
+  (doto (.createElement js/document "label")
+    (aset "innerHTML" (js/Date.))))
+
+;; RENDER
+(defn render [root & components]
+  (doseq [c components]
+    (.append root c)))
+
+(defn ^:export init
+  []
+  (aset (root) "innerHTML" "")
+  (render (root) (new-counter) (new-button-inc) (new-button-reset)))
